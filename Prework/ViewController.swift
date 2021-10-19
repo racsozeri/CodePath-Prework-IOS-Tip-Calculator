c5066cb79b0737d575625ec574142f747329b22b
racsozeri <47546313+racsozeri@users.noreply.github.com>
2021-10-18 23:28:13 -0500

    Update ViewController.swift

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -13,13 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var colorChanger: UISegmentedControl!
+    @IBOutlet weak var billAmount: UILabel!
+    @IBOutlet weak var tip: UILabel!
+    @IBOutlet weak var total: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //Sets the title in the Nagivation Bar
-        self.title = "Tip Calculator"
     }
 
     @IBAction func calculateTip(_ sender: Any) {
@@ -35,6 +37,55 @@ class ViewController: UIViewController {
         tipAmountLabel.text = String(format: "$%.2f", tip)
         //Update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func chooseColor(_ sender: Any) {
+        //Define color options
+        let colorOptions = ["white","black","red"]
+        //Get the color chosen by user
+        let colorChoice = colorOptions[colorChanger.selectedSegmentIndex]
+        
+        //Depending on color chosen, background color and text color will change
+        if (colorChoice == "white"){
+            self.view.backgroundColor = UIColor.white
+            tipAmountLabel.textColor = UIColor.black
+            billAmountTextField.textColor = UIColor.black
+            totalLabel.textColor = UIColor.black
+            tip.textColor = UIColor.black
+            billAmount.textColor = UIColor.black
+            total.textColor = UIColor.black
+            colorChanger.selectedSegmentTintColor = UIColor.white
+            colorChanger.backgroundColor = UIColor.white
+            tipControl.selectedSegmentTintColor = UIColor.white
+            tipControl.backgroundColor = UIColor.white
+        }
+        
+        else if (colorChoice == "black"){
+            self.view.backgroundColor = UIColor.black
+            tipAmountLabel.textColor = UIColor.green
+            billAmountTextField.textColor = UIColor.green
+            totalLabel.textColor = UIColor.green
+            tip.textColor = UIColor.green
+            billAmount.textColor = UIColor.green
+            total.textColor = UIColor.green
+            colorChanger.selectedSegmentTintColor = UIColor.white
+            colorChanger.backgroundColor = UIColor.green
+            tipControl.selectedSegmentTintColor = UIColor.white
+            tipControl.backgroundColor = UIColor.green
+        }
+        
+        else if (colorChoice == "red"){
+            self.view.backgroundColor = UIColor.red
+            tipAmountLabel.textColor = UIColor.black
+            billAmountTextField.textColor = UIColor.black
+            totalLabel.textColor = UIColor.black
+            tip.textColor = UIColor.black
+            billAmount.textColor = UIColor.black
+            total.textColor = UIColor.black
+            colorChanger.selectedSegmentTintColor = UIColor.white
+            colorChanger.backgroundColor = UIColor.white
+            tipControl.selectedSegmentTintColor = UIColor.white
+            tipControl.backgroundColor = UIColor.white
+        }
     }
     
 }
